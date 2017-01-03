286d15e3a68a1bd28db243c3e99a87511e127e60
Adam Nowicki <novic.one@gmail.com>
2017-01-03 21:17:19 +0100

    support downloading over https

--- src/download.js
+++ src/download.js
@@ -1,36 +1,43 @@
-var q = require("q");
-var fs = require("fs");
-var path = require("path");
-var http = require("follow-redirects").http;
+const q = require("q");
+const fs = require("fs");
+const path = require("path");
+const { http, https } = require("follow-redirects");
 
 module.exports = function download(url, path) {
     console.log("downloading %s to %s", url, path);
-    var deferred = q.defer();
+    const deferred = q.defer();
     
     ensureDir(path);
-    var file = fs.createWriteStream(path);
+    const file = fs.createWriteStream(path);
     file.on("error", deferred.reject);
     
-    http.get(url, function(response) {
-        if (response.statusCode >= 400) {
-            deferred.reject(new Error("Unexpected status while downloading file: " + response.statusCode));
-            return;
-        }
+    get(url, (response) => {
+            if (response.statusCode >= 400) {
+                deferred.reject(new Error("Unexpected status while downloading file: " + response.statusCode));
+                return;
+            }
 
-        response.pipe(file);
-        
-        file.on("finish", deferred.resolve);
-    }).on("error", deferred.reject);
+            response.pipe(file);
+            
+            file.on("finish", deferred.resolve);
+        })
+        .on("error", deferred.reject);
     
     return deferred.promise;
 };
 
 function ensureDir(filePath) {
-    var dirPath = path.dirname(filePath);
+    const dirPath = path.dirname(filePath);
     
     if (fs.existsSync(dirPath)) {
         return;
     }
     ensureDir(dirPath);
     fs.mkdirSync(dirPath);
+}
+
+function get(url, handler) {
+    const protocol = url.startsWith("https") ? https : http;
+
+    return protocol.get(url, handler);
 }
